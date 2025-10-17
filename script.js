e297e2361bad3d7839074fca1052b0ba8b60f75a
vineeth42 <33409768+vineeth42@users.noreply.github.com>
2025-10-17 19:00:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const imageUrl = urlParams.get('url') || 'https://via.placeholder.com/300x200?text=Captcha';
-    document.getElementById('captchaImage').src = imageUrl;
+document.addEventListener('DOMContentLoaded', function() {
+    var defaultImageUrl = 'https://via.placeholder.com/300x100?text=Captcha';
+    var imageUrl = new URLSearchParams(window.location.search).get('url');
+    document.getElementById('captchaImage').src = imageUrl || defaultImageUrl;
 });
